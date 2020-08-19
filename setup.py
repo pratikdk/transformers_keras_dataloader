091be7abba1431727a31334af8f417cbcde94993
Pratik Deoolwadikar <pratik.deoolwadikar@gmail.com>
2020-08-19 20:47:21 +0530

    import fix

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ with open('requirements.txt') as f:
 
 setup(
     name='transformers_keras_dataloader',
-    version='0.1',
+    version='0.2',
     author="Pratik Deoolwadikar",
     author_email="pratik.deoolwadikar@gmail.com",
     description="Transformers Keras Dataloader provides an EmbeddingDataLoader class, a subclass of keras.utils.Sequence which enables real-time embedding generation from pretrained transformer models while feeding it to your Keras model via batches.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url = 'https://github.com/pratikdk/transformers_keras_dataloader',
+    download_url = 'https://github.com/pratikdk/transformers_keras_dataloader/archive/v0.1.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
